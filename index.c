dd02d133fb4a03205864d2764ffcf8f32eb84feb
josemarceloisidorio@gmail.com <josemarceloisidorio@gmail.com>
2019-09-15 12:44:29 -0300

    Introdução a estrutura de atribuições de valores e atributos do Pokemon Inicial

--- index.c
+++ index.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-char UserName;
+char UserName, P_atual, TP_atual;
+int Op, FP_atual, VP_atual, RP_atual;
 
 void EscolhaInicial();
 
@@ -43,6 +44,45 @@ void EscolhaInicial() {
 	printf("Velocidade: 3\n");
 	printf("Resistencia: 4\n");
 	printf("Tipo: Aquatico\n");
+	
+	printf("\n===========\n");
+	printf("Escolha: ");
+	scanf("%d", &Op);
+	
+	switch(Op) {
+		case 1:
+			P_atual = "Bulbasaur";
+			FP_atual = 3;
+			VP_atual = 2;
+			RP_atual = 4;
+			TP_atual = "Grama e Veneno";
+			
+			break;
+			
+		case 2:
+			P_atual = "Charmander";
+			FP_atual = 4;
+			VP_atual = 4;
+			RP_atual = 3;
+			TP_atual = "Fogo";
+			
+			break;
+			
+		case 3:
+			P_atual = "Squirtle";
+			FP_atual = 4;
+			VP_atual = 3;
+			RP_atual = 4;
+			TP_atual = "Aquatico";
+			
+			break;
+		
+		default:
+			printf("Ocorreu algum erro...\n");
+			return 1;
+			
+			break;	
+	}
 	
 }
 //Fim da função EscolhaInicial
