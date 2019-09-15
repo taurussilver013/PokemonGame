eb08638d1bd46aef1c36df9456b4767b99a4e1bd
MarceloPrgJr <josemarceloisidorio@gmail.com>
2019-09-15 11:36:08 -0300

    Estrutura básica do programa. Introdução a coleta no UserName

--- index.c
+++ index.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+char UserName;
 
 int main() {
+	printf("==============\n");
+	printf(" POKEMON GAME\n");
+	printf("==============\n");
+	
+	printf("\nDigite seu UserName: ");
+	scanf("%s", &UserName);
+	system("cls");
 	
 	return 0;
 }
+//Fim da função principal
