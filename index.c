22de06d853681f876e46c866d36d7d39dc9e286e
MarceloPrgJr <josemarceloisidorio@gmail.com>
2019-09-15 11:54:58 -0300

    Criação da Função void EscolhaInicial();

--- index.c
+++ index.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 char UserName;
 
+void EscolhaInicial();
+
 int main() {
 	printf("==============\n");
 	printf(" POKEMON GAME\n");
@@ -10,6 +12,37 @@ int main() {
 	scanf("%s", &UserName);
 	system("cls");
 	
+	EscolhaInicial();
+	
 	return 0;
 }
 //Fim da função principal
+
+void EscolhaInicial() {
+	printf("===================\n");
+	printf(" Pokemons Iniciais\n");
+	printf("===================\n\n");
+	
+	printf("\n[1] - Bulbasaur\n");
+	printf("----------------\n");
+	printf("Forca: 3\n");
+	printf("Velocidade: 2\n");
+	printf("Resistencia: 4\n");
+	printf("Tipo: Grama e Veneno\n");
+	
+	printf("\n\n[2] - Charmander\n");
+	printf("------------------\n");
+	printf("Forca: 4\n");
+	printf("Velocidade: 4\n");
+	printf("Resistencia: 3\n");
+	printf("Tipo: Fogo\n");
+	
+	printf("\n\n[3] - Squirtle\n");
+	printf("-----------------\n");
+	printf("Forca: 4\n");
+	printf("Velocidade: 3\n");
+	printf("Resistencia: 4\n");
+	printf("Tipo: Aquatico\n");
+	
+}
+//Fim da função EscolhaInicial
